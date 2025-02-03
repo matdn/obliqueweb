2cbc12b70b0da3baa77554d78e3d5b87d60448ca
Matis DENE <matis.dene@edu.devinci.fr>
2025-02-04 00:21:09 +0100

    update code

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,51 +4,18 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import gsap from "gsap";
 import styles from "../styles/Hero.module.scss";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default function Hero() {
     const [isMounted, setIsMounted] = useState(false);
     const imagesRef = useRef([]);
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            gsap.registerPlugin(ScrollTrigger);
-        }
-        setIsMounted(true);
-    }, []);
 
-    useEffect(() => {
-        if (!isMounted || imagesRef.current.length < 5) return;
-
-        const centralImage = imagesRef.current[0];
-        const surroundingImages = imagesRef.current.slice(1);
-
-        const tl = gsap.timeline();
-
-        tl.fromTo(
-            centralImage,
-            { scale: 0, opacity: 0 },
-            { scale: 1, opacity: 1, duration: 1.5, ease: "power3.out" }
-        );
-
-        tl.fromTo(
-            surroundingImages,
-            { x: 0, y: 0 },
-            {
-                scale: 1,
-                opacity: 1,
-                x: (i) => [-350, 350, -350, 400][i],
-                y: (i) => [-250, -250, 250, 250][i],
-                duration: 1.5,
-            }
-        );
-    }, [isMounted]);
-
-    if (!isMounted) return null; // Évite l'hydratation sur un contenu différent
 
     return (
         <section className={styles.hero}>
             <div className={styles.container}>
-                <Image
+                <img
                     src="/Hero/obliqueHero.svg"
                     alt="Logo"
                     width={300}
@@ -57,7 +24,7 @@ export default function Hero() {
                     className={styles.logo}
                 />
                 {[...Array(5)].map((_, index) => (
-                    <Image
+                    <img
                         key={index}
                         src={`/Hero/HI${index + 1}.png`}
                         alt={`Image ${index + 1}`}
