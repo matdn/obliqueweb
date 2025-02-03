29467541e6d4b03a81b2e296c67dfc12faea7b5c
Matis DENE <matis.dene@edu.devinci.fr>
2025-02-03 19:57:59 +0100

    update code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,11 @@ import styles from "./page.module.css";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import Slider from "./components/Slider";
-// import Sponsors from "./components/Sponsors";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Hero />
+      {/* <Hero /> */}
       {/* <AboutUs />
       <Slider /> */}
       {/* <Sponsors /> */}
